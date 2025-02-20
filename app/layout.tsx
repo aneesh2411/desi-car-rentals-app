aef472de0baf80fc82eab07f92d68dd093d02594
ani2411 <aneeshkalisapudi@gmail.com>
2025-02-19 23:56:14 -0600

    Initial Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,13 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -23,12 +29,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <SignedIn>
+            {children}
+          </SignedIn>
+          <SignedOut>
+            <SignInButton>
+              <button>Sign In</button>
+            </SignInButton>
+          </SignedOut>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
